d4a9064450dc6a93936accca550c11ac7d4abe90
Sakibul Hasan <sakib1514817122@gmail.com>
2025-11-25 23:48:47 +0600

    0417-pacific-atlantic-water-flow Medium 11-25-2025

--- LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,107 +1,47 @@
 class Solution {
 public:
-    vector<vector<bool>> pacific;
-    vector<vector<bool>> atlantic;
-    vector<vector<bool>> visited;
     const vector<pair<int, int>> directions = {
-        {1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-    bool check_atlantic(int cx, int cy, vector<vector<int>>& heights) {
-
-        int row = heights.size();
-        int col = heights[0].size();
-        bool result = false;
-
-        visited[cx][cy] = true;
-
-        // if (atlantic[cx][cy]) {
-        //     return true;
-        // }
-
-        if (cy + 1 == col || cx + 1 == row) {
-            atlantic[cx][cy] = true;
-            return true;
-        }
+    void dfs(int x, int y, vector<vector<int>>& heights,
+             vector<vector<bool>>& reachable) {
+        int m = heights.size();
+        int n = heights[0].size();
+        reachable[x][y] = true;
 
         for (auto [dx, dy] : directions) {
-            int nx = cx + dx;
-            int ny = cy + dy;
-
-            bool valid = (nx >= 0 && ny >= 0 && nx < row && ny < col);
-            if (valid && !visited[nx][ny] &&
-                heights[nx][ny] <= heights[cx][cy]) {
-
-                if (atlantic[nx][ny]) {
-                    return true;
-                }
-                result = check_atlantic(nx, ny, heights);
-                if (result) {
-                    return result;
-                }
+            int nx = x + dx;
+            int ny = y + dy;
+            if (nx >= 0 && ny >= 0 && nx < m && ny < n && !reachable[nx][ny] &&
+                heights[nx][ny] >= heights[x][y]) {
+                dfs(nx, ny, heights, reachable);
             }
         }
-        return result;
-    }
-    bool check_pacific(int cx, int cy, vector<vector<int>>& heights) {
-
-        int row = heights.size();
-        int col = heights[0].size();
-        bool result = false;
-
-        visited[cx][cy] = true;
-
-        // if (pacific[cx][cy]) {
-        //     return true;
-        // }
-
-        if (cy == 0 || cx == 0) {
-            pacific[cx][cy] = true;
-            return true;
-        }
-
-        for (auto [dx, dy] : directions) {
-            int nx = cx + dx;
-            int ny = cy + dy;
-
-            bool valid = (nx >= 0 && ny >= 0 && nx < row && ny < col);
-            if (valid && !visited[nx][ny] &&
-                heights[nx][ny] <= heights[cx][cy]) {
-
-                if (pacific[nx][ny]) {
-                    return true;
-                }
-
-                result = check_pacific(nx, ny, heights);
-                if (result) {
-                    return result;
-                }
-            }
-        }
-        return result;
     }
 
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int i, j;
-        vector<vector<int>> ans;
+        int m = heights.size();
+        int n = heights[0].size();
+        vector<vector<bool>> pacific(m, vector<bool>(n, false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
 
-        int row = heights.size();
-        int col = heights[0].size();
+        for (int i = 0; i < m; i++) {
+            dfs(i, 0, heights, pacific);      // Left border (Pacific)
+            dfs(i, n - 1, heights, atlantic); // Right border (Atlantic)
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(0, j, heights, pacific);      // Top border (Pacific)
+            dfs(m - 1, j, heights, atlantic); // Bottom border (Atlantic)
+        }
 
-        pacific.assign(row, vector<bool>(col, false));
-        atlantic.assign(row, vector<bool>(col, false));
-
-        for (i = 0; i < row; i++) {
-            for (j = 0; j < col; j++) {
-                visited.assign(row, vector<bool>(col, false));
-                bool isAtlantic = check_atlantic(i, j, heights);
-                visited.assign(row, vector<bool>(col, false));
-                bool isPacific = check_pacific(i, j, heights);
-
-                if (isAtlantic && isPacific) {
-                    ans.push_back({i, j});
+        vector<vector<int>> result;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    result.push_back({i, j});
                 }
             }
         }
-        return ans;
+        return result;
     }
 };
