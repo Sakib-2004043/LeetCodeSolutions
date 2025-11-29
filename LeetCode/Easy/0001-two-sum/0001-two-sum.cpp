2ca9b06fe97ac7df69249345b6d6d3e70106b63c
Sakibul Hasan <sakib1514817122@gmail.com>
2025-11-29 23:27:16 +0600

    0001-two-sum Easy 11-29-2025

---
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i, n;
+        map<int, bool> valFind;
+        map<int, int> index;
+        vector<int> retVec;
+
+        n = nums.size();
+        for (i = 0; i < n; i++) {
+            int find = target - nums[i];
+            if (valFind[find]) {
+                retVec.push_back(index[find]);
+                retVec.push_back(i);
+                return retVec;
+            }
+            valFind[nums[i]] = true;
+            index[nums[i]] = i;
+        }
+        return retVec;
+    }
+};
