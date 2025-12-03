e7ade698836e53344d889a059962e049988e1911
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-03 23:39:24 +0600

    12-03-2025 0137-single-number-ii Medium

--- LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.cpp
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int i, n, ans;
-        map<int, int> visited;
-        n = nums.size();
-        for (i = 0; i < n; i++) {
-            visited[nums[i]]++;
-        }
-        for (auto x : visited) {
-            if (x.second == 1) {
-                ans = x.first;
+        ans = 0;
+        for (i = 0; i < 32; i++) {
+            int sum = 0;
+            for (auto x : nums) {
+                sum += ((x >> i) & 1);
             }
+            ans = ans | ((sum % 3) << i);
         }
         return ans;
     }
