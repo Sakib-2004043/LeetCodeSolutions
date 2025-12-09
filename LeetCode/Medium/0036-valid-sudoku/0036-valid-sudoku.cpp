4daa519e9c3bb522010b8c6e22b43c158a6275d8
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-09 14:24:21 +0600

    0036-valid-sudoku Medium 12-09-2025

---
+++ LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int i, j, k, l;
+        map<char, bool> visited;
+
+        for (i = 0; i < 9; i++) {
+            visited.clear();
+            for (j = 0; j < 9; j++) {
+                if (visited[board[i][j]]) {
+                    return false;
+                }
+                if (board[i][j] != '.') {
+                    visited[board[i][j]] = true;
+                }
+            }
+        }
+        for (i = 0; i < 9; i++) {
+            visited.clear();
+            for (j = 0; j < 9; j++) {
+                if (visited[board[j][i]]) {
+                    return false;
+                }
+                if (board[j][i] != '.') {
+                    visited[board[j][i]] = true;
+                }
+            }
+        }
+
+        for (i=0;i<9;i+=3){
+            for(j=0;j<9;j+=3){
+                visited.clear();
+                for(k=i;k<i+3;k++){
+                    for(l=j;l<j+3;l++){
+                        if (visited[board[k][l]]) {
+                            return false;
+                        }
+                        if (board[k][l] != '.') {
+                            visited[board[k][l]] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
