b25a11ae49cfe3dd8d6dbb1d05d763015e8a6836
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-09 13:53:17 +0600

    0071-simplify-path Medium 12-09-2025

---
+++ LeetCode/Medium/0071-simplify-path/0071-simplify-path-gptCode.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        string token;
+        stringstream ss(path);
+
+        while (getline(ss, token, '/')) {
+            if (token == "" || token == ".") continue;
+            if (token == "..") {
+                if (!st.empty()) st.pop_back();
+            } else {
+                st.push_back(token);
+            }
+        }
+
+        if (st.empty()) return "/";
+
+        string res = "";
+        for (auto &dir : st)
+            res += "/" + dir;
+
+        return res;
+    }
+};
