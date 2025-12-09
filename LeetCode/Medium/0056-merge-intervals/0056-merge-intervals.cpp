9ce68103e4b630961df00a23dcd39877aff1949c
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-09 21:11:49 +0600

    0056-merge-intervals Medium 12-09-2025

--- LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.cpp
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int i, n;
+        int n = intervals.size();
+        vector<bool> merged(n, false); // track merged intervals
         vector<vector<int>> ans;
-        n = intervals.size();
-        // sort(intervals.begin(),intervals.end());
-        ans.push_back(intervals[0]);
-        for (i = 1; i < n; i++) {
-            vector<int> newPair = intervals[i];
-            vector<int> ansPair = ans.back();
-            if (ansPair[1] >= newPair[1] && ansPair[0] <= newPair[0]) {
-                continue;
-            }
-            if (ansPair[0] > newPair[0]) {
-                swap(ansPair, newPair);
-            }
 
-            if (ansPair[1] >= newPair[0]) {
-                ans.pop_back();
-                ans.push_back(
-                    {min(ansPair[0], newPair[0]), max(ansPair[1], newPair[1])});
-            } else if (ansPair[1] < newPair[0]) {
-                ans.push_back(newPair);
+        for (int i = 0; i < n; i++) {
+            if (merged[i]) continue;
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            
+            bool changed = true;
+            while (changed) {
+                changed = false;
+                for (int j = 0; j < n; j++) {
+                    if (i == j || merged[j]) continue;
+                    int s = intervals[j][0], e = intervals[j][1];
+                    
+                    // check if current interval overlaps with interval[j]
+                    if (!(end < s || start > e)) {
+                        start = min(start, s);
+                        end = max(end, e);
+                        merged[j] = true;
+                        changed = true;
+                    }
+                }
             }
+            
+            ans.push_back({start, end});
+            merged[i] = true;
         }
 
         return ans;
