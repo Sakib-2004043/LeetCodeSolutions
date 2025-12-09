6b72c7f70f1bfbec34cdf31531ac2ed938f70874
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-09 21:11:41 +0600

    0056-merge-intervals Medium 12-09-2025

--- LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.cpp
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,23 +4,25 @@ public:
         int i, n;
         vector<vector<int>> ans;
         n = intervals.size();
-        sort(intervals.begin(),intervals.end());
+        // sort(intervals.begin(),intervals.end());
         ans.push_back(intervals[0]);
         for (i = 1; i < n; i++) {
             vector<int> newPair = intervals[i];
             vector<int> ansPair = ans.back();
-            
-            if(ansPair[1] >= newPair[1]){
+            if (ansPair[1] >= newPair[1] && ansPair[0] <= newPair[0]) {
                 continue;
             }
-            else if(ansPair[1] >= newPair[0]){
-                ans.pop_back();
-                ans.push_back({ansPair[0],newPair[1]});
-            }
-            else if(ansPair[1] < newPair[0]){
-                ans.push_back(newPair);
+            if (ansPair[0] > newPair[0]) {
+                swap(ansPair, newPair);
             }
 
+            if (ansPair[1] >= newPair[0]) {
+                ans.pop_back();
+                ans.push_back(
+                    {min(ansPair[0], newPair[0]), max(ansPair[1], newPair[1])});
+            } else if (ansPair[1] < newPair[0]) {
+                ans.push_back(newPair);
+            }
         }
 
         return ans;
