9917c0793f7bf8ce59cd03d89f7c314460f17232
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-09 13:52:23 +0600

    0071-simplify-path Medium 12-09-2025

--- LeetCode/Medium/0071-simplify-path/0071-simplify-path.cpp
+++ LeetCode/Medium/0071-simplify-path/0071-simplify-path.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        int i, j, n;
-        stack<string> st;
-        string ret = "";
-        n = path.size();
-        for (i = 0; i < n; i++) {
-            string temp = "";
-            while (i < n && path[i] != '/') {
-                temp += path[i];
-                i++;
-            }
-            if (temp == ".") {
-                continue;
-            } else if (temp == ".." && !st.empty()) {
-                st.pop();
-            } else if (temp != "" && temp != "..") {
-                st.push(temp);
+        vector<string> st;
+        string token;
+        stringstream ss(path);
+
+        while (getline(ss, token, '/')) {
+            if (token == "" || token == ".") continue;
+            if (token == "..") {
+                if (!st.empty()) st.pop_back();
+            } else {
+                st.push_back(token);
             }
         }
-        while (!st.empty()) {
-            cout << "/" << st.top();
-            ret = "/" + st.top() + ret;
-            st.pop();
-        }
-        if (ret == "") {
-            ret = "/";
-        }
-        return ret;
+
+        if (st.empty()) return "/";
+
+        string res = "";
+        for (auto &dir : st)
+            res += "/" + dir;
+
+        return res;
     }
 };
