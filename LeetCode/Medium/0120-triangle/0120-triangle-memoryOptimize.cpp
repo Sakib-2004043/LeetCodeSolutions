4b2b8756efd0a897ac341c98d4f6fa91a27a1e4b
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-14 21:10:54 +0600

    0120-triangle Medium 12-14-2025

---
+++ LeetCode/Medium/0120-triangle/0120-triangle-memoryOptimize.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> preSum = triangle[0];
+        vector<int> newSum;
+        newSum.reserve(n);
+
+        for (int i = 1; i < n; i++) {
+            newSum.clear();
+
+            for (int j = 0; j <= i; j++) {
+                int best = INT_MAX;
+                if (j < preSum.size())
+                    best = preSum[j];
+                if (j > 0)
+                    best = min(best, preSum[j - 1]);
+
+                newSum.push_back(best + triangle[i][j]);
+            }
+
+            preSum.swap(newSum);
+        }
+
+        return *min_element(preSum.begin(), preSum.end());
+    }
+};
