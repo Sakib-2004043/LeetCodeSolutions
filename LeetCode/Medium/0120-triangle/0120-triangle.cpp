2a9a4fffda6293d288f7cc2e88209a44e7d34ce4
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-14 21:06:27 +0600

    0120-triangle Medium 12-14-2025

--- LeetCode/Medium/0120-triangle/0120-triangle.cpp
+++ LeetCode/Medium/0120-triangle/0120-triangle.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int i, j, n, m, k, ans;
-        vector<int> preSum;
-        n = triangle.size();
-        if (n == 1) {
-            return triangle[0][0];
-        }
-        preSum = triangle[0];
-        ans = 0;
-        for (i = 1; i < n; i++) {
-            vector<int> newSum;
-            m = preSum.size();
-            k = n%2;
-            for (j = 0; j < m; j++) {
-                newSum.push_back(preSum[j] + triangle[i][j]);
-                if (k) {
-                    newSum.push_back(preSum[j] + triangle[i][j + 1]);
-                    k = 0;
-                } else {
-                    k = 1;
-                    newSum.back() =
-                        min(newSum.back(), preSum[j] + triangle[i][j + 1]);
-                }
+        int n = triangle.size();
+
+        vector<int> preSum = triangle[0]; 
+
+        for (int i = 1; i < n; i++) {
+            vector<int> newSum(i + 1, INT_MAX);
+
+            for (int j = 0; j <= i; j++) {
+                if (j > 0)
+                    newSum[j] = min(newSum[j], preSum[j - 1] + triangle[i][j]);
+
+                if (j < preSum.size())
+                    newSum[j] = min(newSum[j], preSum[j] + triangle[i][j]);
             }
-            ans = *min_element(newSum.begin(),newSum.end());
-            for (auto x : newSum) {
-                cout << x << " ";
-            }
-            cout << endl;
+
             preSum = newSum;
         }
-        return ans;
+
+        return *min_element(preSum.begin(), preSum.end());
     }
 };
