a595676e1b37c4ddb2eec3f0df07bf84a49c7d52
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-14 19:56:46 +0600

    0300-longest-increasing-subsequence Medium 12-14-2025

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> lis(n, 1);
-        int ans = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    lis[i] = max(lis[i], lis[j] + 1);
-                }
+        vector<int> tail;
+
+        for (int x : nums) {
+            auto it = lower_bound(tail.begin(), tail.end(), x);
+            if (it == tail.end()) {
+                tail.push_back(x);
+            } else {
+                *it = x;
             }
-            ans = max(ans, lis[i]);
         }
-        return ans;
+        return tail.size();
     }
 };
