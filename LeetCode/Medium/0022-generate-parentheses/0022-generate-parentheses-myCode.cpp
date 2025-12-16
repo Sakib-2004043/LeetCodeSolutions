3fac2412c840c3fafc232f413d97a64237a33109
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-16 14:58:39 +0600

    0022-generate-parentheses Medium 12-16-2025

---
+++ LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses-myCode.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        int i;
+        string par;
+        vector<string> ans;
+        for (i = 0; i < n; i++) {
+            par = "("+par+")";
+        }
+        do {
+            int left = 0, right = 0, flag = 1;
+            for (i = 0; i < n * 2; i++) {
+                if (par[i] == '(')
+                    left++;
+                else
+                    right++;
+                if (left < right)
+                    flag = 0;
+            }
+            if (flag)
+                ans.push_back(par);
+            // cout<<par<<endl;
+        } while (next_permutation(par.begin(), par.end()));
+        return ans;
+    }
+};
