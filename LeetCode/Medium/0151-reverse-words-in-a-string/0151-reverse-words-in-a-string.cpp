c3aded9c9322160d5bc24bca7d49711a5d6f014a
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-20 19:11:23 +0600

    0151-reverse-words-in-a-string Medium 12-20-2025

--- LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int i, j, n;
-        string ans = "";
-        n = s.size();
-        for (i = 0, j = 0; i < n; i++) {
-            string temp = "";
-            while (i < n && !isspace(s[i])) {
-                temp = temp + s[i];
-                i++;
-            }
-            if(temp == ""){
-                continue;
-            }
-            if (ans == "") {
-                ans += temp;
-            } else
-                ans = temp + " " + ans;
+        vector<string> words;
+        int n = s.size(), i = 0;
+
+        while (i < n) {
+            // Skip spaces
+            while (i < n && isspace(s[i])) i++;
+            if (i >= n) break;
+
+            // Collect a word
+            int j = i;
+            while (j < n && !isspace(s[j])) j++;
+            words.push_back(s.substr(i, j - i));
+            i = j;
         }
+
+        // Build reversed string
+        string ans = "";
+        for (int k = words.size() - 1; k >= 0; k--) {
+            ans += words[k];
+            if (k != 0) ans += " ";
+        }
+
         return ans;
     }
 };
