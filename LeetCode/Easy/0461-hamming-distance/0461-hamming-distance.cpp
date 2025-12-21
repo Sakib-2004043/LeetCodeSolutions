36a5d34cb3751f8ab3b16515465942ad09c331e4
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-21 17:25:02 +0600

    0461-hamming-distance Easy 12-21-2025

--- LeetCode/Easy/0461-hamming-distance/0461-hamming-distance.cpp
+++ LeetCode/Easy/0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
+        int diff = x ^ y;
         int ans = 0;
-        for (int i = 0; i < 32; i++) {
-            if ( ((x >> i) & 1) ^ ((y >> i) & 1) ) {
-                ans++;
-            }
+        while (diff) {
+            ans += diff & 1;
+            diff >>= 1;
         }
         return ans;
     }
