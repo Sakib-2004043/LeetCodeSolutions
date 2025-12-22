98953f1f7171f91d57fdf0b8e767a215c2d1c3b0
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-22 18:31:43 +0600

    0162-find-peak-element Medium 12-22-2025

--- LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.cpp
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if (nums.size() == 1) {
-            return 0;
-        }
-        if (nums[0] > nums[1]) {
-            return 0;
-        }
-        if (nums[nums.size() - 1] > nums[nums.size() - 2]) {
-            return nums.size() - 1;
-        }
-        int i, bgn, mid, end;
-        bgn = 1;
-        end = nums.size() - 2;
-        mid = bgn + (end - bgn) / 2;
-        while (bgn <= end) {
-            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
-                return mid;
-            }
-            if (nums[mid - 1] > nums[mid]) {
-                end = mid - 1;
-                mid = bgn + (end - bgn) / 2;
+        int low = 0, high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                // peak is on the right side
+                low = mid + 1;
             } else {
-                bgn = mid + 1;
-                mid = bgn + (end - bgn) / 2;
+                // peak is on the left side or at mid
+                high = mid;
             }
         }
-        return 0;
+        return low;  // or high (same)
     }
 };
