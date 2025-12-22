0a05a4fb0c9d1fa4f3958c0bba654a35789aa06a
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-22 23:18:31 +0600

    0034-find-first-and-last-position-of-element-in-sorted-array Medium 12-22-2025

--- LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,61 +2,35 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans = {-1, -1};
-        int bgn, end, mid, pos;
-        bgn = 0, pos = -1;
-        end = nums.size() - 1;
-        while (bgn <= end) {
-            mid = bgn + (end - bgn) / 2;
-            if (nums[mid] == target) {
-                pos = mid;
-                break;
-            }
-            if (target < nums[mid]) {
-                end = mid - 1;
-            } else {
-                bgn = mid + 1;
-            }
+        int n = nums.size();
+        if (n == 0) return ans;
+
+        // Find first occurrence
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target)
+                right = mid - 1;
+            else
+                left = mid + 1;
         }
-        if(pos==-1){
+
+        if (left == n || nums[left] != target)
             return ans;
+
+        ans[0] = left;
+
+        // Find last occurrence
+        right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
-        if(nums[0]==target){
-            ans[0]=0;
-            goto ff;
-        }
-        bgn = 0;
-        end = pos;
-        while (bgn <= end) {
-            mid = bgn + (end - bgn) / 2;
-            if (nums[mid] != target && nums[mid + 1] == target) {
-                ans[0] = mid + 1;
-                break;
-            }
-            if (nums[mid] == target) {
-                end = mid - 1;
-            } else
-                bgn = mid + 1;
-        }
-        ff:;
-        if(nums[nums.size()-1]==target){
-            ans[1]=nums.size()-1;
-            goto ss;
-        }
-        bgn = pos;
-        end = nums.size()-1;
-        while (bgn <= end) {
-            mid = bgn + (end - bgn) / 2;
-            mid = mid ? mid:1;
-            if (nums[mid-1] == target && nums[mid] != target) {
-                ans[1] = mid-1;
-                break;
-            }
-            if (nums[mid] == target) {
-                bgn = mid + 1;
-            } else
-                end = mid - 1;
-        }
-        ss:;
+
+        ans[1] = right;
         return ans;
     }
 };
