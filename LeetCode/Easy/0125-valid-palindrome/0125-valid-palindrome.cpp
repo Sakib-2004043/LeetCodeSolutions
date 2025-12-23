dbc06fc933275f3014dd522c37fb77beb67afbaa
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-23 11:51:52 +0600

    0125-valid-palindrome Easy 12-23-2025

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i, bgn, end;
-        i = 0;
-        while (i < s.size()) {
-            if (!isalnum(s[i])) {
-                s.erase(i, 1);
-            } else {
-                s[i] = tolower(s[i]);
-                i++;
-            }
-        }
-        cout<<s;
-        bgn = 0;
-        end = s.size() - 1;
-        while (bgn <= end) {
-            if (s[bgn] != s[end]) {
+        int bgn = 0, end = s.size() - 1;
+
+        while (bgn < end) {
+            // skip non-alphanumeric from left
+            while (bgn < end && !isalnum((unsigned char)s[bgn]))
+                bgn++;
+
+            // skip non-alphanumeric from right
+            while (bgn < end && !isalnum((unsigned char)s[end]))
+                end--;
+
+            if (tolower((unsigned char)s[bgn]) !=
+                tolower((unsigned char)s[end])) {
                 return false;
             }
+
             bgn++;
             end--;
         }
