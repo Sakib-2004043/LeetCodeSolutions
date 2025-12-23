cb6479d7b8884f378974e094d42fd815c6b63704
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-24 01:13:58 +0600

    0219-contains-duplicate-ii Easy 12-23-2025

--- LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         int i, n;
+        unordered_map<int, int> mp;
         n = nums.size();
-        for (i = 0; i + k < n; i++) {
-            if (nums[i] == nums[i + k]) {
+        for (i = 0; i < n; i++) {
+            if (mp[nums[i]] && (i - mp[nums[i]]) < k) {
                 return true;
             }
+            mp[nums[i]] = i + 1;
         }
         return false;
     }
