f8098ea5d5dc0916df5565aca54612bba44a744a
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-24 16:40:12 +0600

    0200-number-of-islands Medium 12-24-2025

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,21 +3,19 @@ public:
     int n, m;
     vector<vector<bool>> visited;
 
-    const vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
+    const vector<pair<int, int>> directions = {
+        {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     void dfs(int x, int y, vector<vector<char>>& grid) {
         visited[x][y] = true;
         for (auto [dx, dy] : directions) {
             int nx = x + dx;
             int ny = y + dy;
-
-            if (nx >= 0 && ny >= 0 && nx < n && ny < m &&
-                !visited[nx][ny] && grid[nx][ny] == '1') {
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] &&
+                grid[nx][ny] == '1') {
                 dfs(nx, ny, grid);
             }
         }
     }
-
     int numIslands(vector<vector<char>>& grid) {
         n = grid.size();
         m = grid[0].size();
