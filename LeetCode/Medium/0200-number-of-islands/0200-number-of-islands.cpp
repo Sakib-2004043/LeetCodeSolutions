9ad88ce356853a058f9945930f356ed7ac9b2786
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-24 16:40:06 +0600

    0200-number-of-islands Medium 12-24-2025

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,30 +1,32 @@
-map<int, map<int, bool>> visited;
-const vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-
-void dfs(int row, int col, vector<vector<char>>& grid) {
-    int rmax = grid.size();
-    int cmax = grid[0].size();
-    visited[row][col] = true;
-    for (auto [dx, dy] : directions) {
-        int nx = row + dx;
-        int ny = col + dy;
-        bool valid = (nx >= 0 && ny >= 0 && nx < rmax && ny < cmax);
-        if (valid && !visited[nx][ny] && grid[nx][ny] == '1') {
-            dfs(nx, ny, grid);
-        }
-    }
-}
-
 class Solution {
 public:
+    int n, m;
+    vector<vector<bool>> visited;
+
+    const vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+    void dfs(int x, int y, vector<vector<char>>& grid) {
+        visited[x][y] = true;
+        for (auto [dx, dy] : directions) {
+            int nx = x + dx;
+            int ny = y + dy;
+
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m &&
+                !visited[nx][ny] && grid[nx][ny] == '1') {
+                dfs(nx, ny, grid);
+            }
+        }
+    }
+
     int numIslands(vector<vector<char>>& grid) {
-        int i, j, r, c, components;
-        visited.clear();
-        r = grid.size();
-        c = grid[0].size();
-        components = 0;
-        for (i = 0; i < r; i++) {
-            for (j = 0; j < c; j++) {
+        n = grid.size();
+        m = grid[0].size();
+        visited.assign(n, vector<bool>(m, false));
+
+        int components = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (!visited[i][j] && grid[i][j] == '1') {
                     components++;
                     dfs(i, j, grid);
