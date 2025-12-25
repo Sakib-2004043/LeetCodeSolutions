80bf8c2bb9b8d450a087b95f2e8fc0eab26d2925
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-25 18:10:19 +0600

    0274-h-index Medium 12-25-2025

--- LeetCode/Medium/0274-h-index/0274-h-index.cpp
+++ LeetCode/Medium/0274-h-index/0274-h-index.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int i, n, ans;
-        n = citations.size();
         sort(citations.rbegin(), citations.rend());
-        ans = 0;
-        for (i = 0; i < n; i++) {
+        int ans = 0;
+        for (int i = 0; i < citations.size(); i++) {
             if (citations[i] > ans) {
                 ans++;
             }
