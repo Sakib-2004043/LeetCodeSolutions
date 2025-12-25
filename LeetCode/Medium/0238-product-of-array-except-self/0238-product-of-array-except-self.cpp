abd1c06268c335b3b721f2bd0d5621f8c5ebce8a
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-25 19:22:30 +0600

    0238-product-of-array-except-self Medium 12-25-2025

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,12 +3,13 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n, 1);
+        
         int suffix = 1;
         for (int i = 0; i < n; i++) {
             ans[i] = suffix;
             suffix *= nums[i];
         } // Ignores Last Element
-        
+
         int prefix = 1;
         for (int i = n - 1; i >= 0; i--) {
             ans[i] *= prefix;
