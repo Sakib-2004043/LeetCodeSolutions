cd7bf7092cc67b519e9a30c8c39c3a160d34df5b
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-26 17:29:26 +0600

    0134-gas-station Medium 12-26-2025

---
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int i, sum, ans, mx;
+        vector<int> suffix_sum;
+        sum = 0, mx = 0;
+        for (i = 0; i < gas.size(); i++) {
+            cout << gas[i] - cost[i] << " ";
+            suffix_sum.push_back(gas[i] - cost[i]);
+            sum += (gas[i] - cost[i]);
+        }
+        for (i = gas.size() - 2; i >= 0; i--) {
+            suffix_sum[i] += suffix_sum[i + 1];
+        }
+        ans = max_element(suffix_sum.begin(), suffix_sum.end()) -
+              suffix_sum.begin();
+        return sum >= 0 ? ans : -1;
+    }
+};
