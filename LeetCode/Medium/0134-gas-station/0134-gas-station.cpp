98ec6570711c133f65891ae71a4a20ab1acc5e83
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-26 18:07:17 +0600

    0134-gas-station Medium 12-26-2025

--- LeetCode/Medium/0134-gas-station/0134-gas-station.cpp
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int i, sum, ans;
+        int sum, ans, mx, n;
         vector<int> suffix_sum;
+        n = gas.size();
         sum = 0;
-        for (i = 0; i < gas.size(); i++) {
+        for (int i = 0; i < n; i++) {
             // cout << gas[i] - cost[i] << " ";
             suffix_sum.push_back(gas[i] - cost[i]);
             sum += (gas[i] - cost[i]);
         }
-        for (i = gas.size() - 2; i >= 0; i--) {
-            suffix_sum[i] += suffix_sum[i + 1];
+        if (sum < 0) {
+            return -1;
         }
-        ans = max_element(suffix_sum.begin(), suffix_sum.end()) -
-              suffix_sum.begin();
-        return sum >= 0 ? ans : -1;
+        mx = suffix_sum[n - 1];
+        ans = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            suffix_sum[i] += suffix_sum[i + 1];
+            if (suffix_sum[i] >= mx) {
+                mx = suffix_sum[i];
+                ans = i;
+            }
+        }
+        return ans;
     }
 };
