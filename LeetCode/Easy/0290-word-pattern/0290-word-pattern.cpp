5edc9d3ab3a79b6f95ed64de03266c2786e8816e
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-26 21:56:41 +0600

    0290-word-pattern Easy 12-26-2025

--- LeetCode/Easy/0290-word-pattern/0290-word-pattern.cpp
+++ LeetCode/Easy/0290-word-pattern/0290-word-pattern.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        int i, j, n;
-        unordered_map<string, char> s_c;
-        unordered_map<char, string> c_s;
-        n = s.size();
-        i = 0, j = 0;
-        while (i < n) {
-            string temp = "";
-            if (!isalpha(s[i])) {
-                i++;
-                continue;
-            }
-            while (i < n && isalpha(s[i])) {
-                temp += s[i];
-                i++;
-            }
-            if (s_c.count(temp) == 0) {
-                s_c[temp] = pattern[j];
-            } else if (s_c[temp] != pattern[j]) {
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        stringstream ss(s);
+        string word;
+        int i = 0;
+        while (ss >> word) {
+            if (i >= pattern.size())
                 return false;
-            }
-            if (c_s.count(pattern[j]) == 0) {
-                c_s[pattern[j]] = temp;
-            } else if (c_s[pattern[j]] != temp) {
+            char c = pattern[i];
+            if (charToWord.count(c) && charToWord[c] != word)
                 return false;
-            }
-            j++;
+            if (wordToChar.count(word) && wordToChar[word] != c)
+                return false;
+            charToWord[c] = word;
+            wordToChar[word] = c;
+            i++;
         }
-        if(i==n && j==pattern.size()){
-            return true;
-        }
-        return false;;
+        return i == pattern.size();
     }
 };
