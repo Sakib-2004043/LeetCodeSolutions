96f593dd6045099c157ea99401779388461d4555
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-27 23:26:25 +0600

    0050-powx-n Medium 12-27-2025

--- LeetCode/Medium/0050-powx-n/0050-powx-n.cpp
+++ LeetCode/Medium/0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
+    double myPow(double x, long long n) {
+        double result = 1.0;
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        while (n) {
+            if (n & 1) {
+                result *= x;
+            }
+            x*=x;
+            n >>= 1;
+        }
+        return result;
     }
 };
