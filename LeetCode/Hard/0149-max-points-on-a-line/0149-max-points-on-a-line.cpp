fd9b404bdf39089e8fded1dfd5be883af58ebb6f
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-27 22:04:02 +0600

    0149-max-points-on-a-line Hard 12-27-2025

--- LeetCode/Hard/0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ LeetCode/Hard/0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        int i, j, n, ans;
-        n = points.size();
-        ans = 0;
-        for (i = 0; i < n; i++) {
-            map<pair<double, double>, int> visitedPoint;
-            for (j = 0; j < n; j++) {
-                if (i == j) {
+        int n = points.size();
+        if (n <= 2)
+            return n;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            unordered_map<string, int> slopeCount;
+            int curMax = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j)
                     continue;
+                int dx = points[j][0] - points[i][0];
+                int dy = points[j][1] - points[i][1];
+
+                int g = gcd(dy, dx);
+                if (g != 0) {
+                    dy /= g;
+                    dx /= g;
                 }
-                double x1 = points[i][0], y1 = points[i][1];
-                double x2 = points[j][0], y2 = points[j][1];
-                double slope = (y2 - y1) / (x2 - x1);
-                double yCut = (x1 * y2 - x2 * y1) / (x1 - x2);
-                visitedPoint[{slope, yCut}]++;
-                ans = max(ans, visitedPoint[{slope, yCut}]);
+                string key = to_string(dy) + "/" + to_string(dx);
+                slopeCount[key]++;
+                curMax = max(curMax, slopeCount[key]);
             }
+            ans = max(ans, curMax + 1); // +1 for the point itself
         }
-        return ++ans;
+        return ans;
     }
 };
