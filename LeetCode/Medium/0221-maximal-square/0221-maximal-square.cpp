4b8f1935b982422d531bd7b052031ad439936bb2
Sakibul Hasan <sakib1514817122@gmail.com>
2025-12-27 16:40:09 +0600

    0221-maximal-square Medium 12-27-2025

---
+++ LeetCode/Medium/0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int i, j, k, l, row, col, ans;
+        row = matrix.size();
+        col = matrix[0].size();
+        vector<vector<int>> area(row, vector<int>(col, 0));
+        ans = 0;
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < col; j++) {
+                if (i == 0 || j == 0) {
+                    area[i][j] = matrix[i][j] - '0';
+                    ans = max(ans, area[i][j]);
+                    continue;
+                }
+                int mn = min(area[i - 1][j], area[i][j - 1]);
+                mn = min(mn, area[i - 1][j - 1]);
+                area[i][j] = mn + 1;
+                ans = max(ans, mn);
+            }
+        }
+        return ans * ans;
+    }
+};
