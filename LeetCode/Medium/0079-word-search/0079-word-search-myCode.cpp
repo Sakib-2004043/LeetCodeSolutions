76802beb5dcd2e306c9e9c8e834e3fdda552b399
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-02 22:54:04 +0600

    0079-word-search Medium 01-02-2026

---
+++ LeetCode/Medium/0079-word-search/0079-word-search-myCode.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private:
+    string targetWord;
+    bool ans = false;
+    vector<vector<bool>> visited;
+
+public:
+    void search(vector<vector<char>>& board, string curWord, int row, int col) {
+        if (curWord == targetWord) {
+            ans = true;
+            return;
+        }
+        if (curWord[curWord.size()-1] != targetWord[curWord.size()-1]) {
+            return;
+        }
+        visited[row][col] = true;
+        if (row + 1 < board.size() && !visited[row + 1][col]) {
+            search(board, curWord + board[row + 1][col], row + 1, col);
+        }
+        if (row - 1 >= 0 && !visited[row - 1][col]) {
+            search(board, curWord + board[row - 1][col], row - 1, col);
+        }
+        if (col + 1 < board[0].size() && !visited[row][col + 1]) {
+            search(board, curWord + board[row][col + 1], row, col + 1);
+        }
+        if (col - 1 >= 0 && !visited[row][col - 1]) {
+            search(board, curWord + board[row][col - 1], row, col - 1);
+        }
+        visited[row][col] = false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        this->targetWord = word;
+        int row = board.size();
+        int col = board[0].size();
+        visited.assign(row, vector<bool>(col, false));
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                search(board, string(1, board[i][j]), i, j);
+            }
+        }
+        return ans;
+    }
+};
