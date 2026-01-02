d5a7af03f5b63f8ceab18c67eaba5c44b2b06bb3
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-02 11:09:11 +0600

    0046-permutations Medium 01-02-2026

--- LeetCode/Medium/0046-permutations/0046-permutations.cpp
+++ LeetCode/Medium/0046-permutations/0046-permutations.cpp
@@ -1,11 +1,21 @@
 class Solution {
+private:
+    void getPerms(vector<int>& nums, int idx, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[i], nums[idx]);
+            getPerms(nums, idx + 1, ans);
+            swap(nums[i], nums[idx]);
+        }
+    }
+
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        do {
-            ans.push_back(nums);
-        } while (next_permutation(nums.begin(), nums.end()));
+        getPerms(nums, 0, ans);
         return ans;
     }
 };
