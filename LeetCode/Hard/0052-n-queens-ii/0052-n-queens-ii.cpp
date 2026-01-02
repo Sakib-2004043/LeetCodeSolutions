d0a76c832350d9e006ad313a606848a85da793c4
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-02 21:49:49 +0600

    0052-n-queens-ii Hard 01-02-2026

--- LeetCode/Hard/0052-n-queens-ii/0052-n-queens-ii.cpp
+++ LeetCode/Hard/0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,27 +1,44 @@
 class Solution {
+private:
+    vector<vector<string>> ans;
+    bool isSafe(vector<string>& board, int row, int col) {
+        int n = board.size();
+        for (int i = 0; i < n; i++) {
+            if (board[row][i] == 'Q' || board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    void makePosition(vector<string>& board, int row) {
+        int n = board.size();
+        if(row == n){
+            ans.push_back(board);
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                makePosition(board, row + 1);
+            }
+            board[row][col] = '.';
+        }
+    }
+
 public:
     int totalNQueens(int n) {
-        switch (n) {
-        case 1:
-            return 1;
-        case 2:
-            return 0;
-        case 3:
-            return 0;
-        case 4:
-            return 2;
-        case 5:
-            return 10;
-        case 6:
-            return 4;
-        case 7:
-            return 40;
-        case 8:
-            return 92;
-        case 9:
-            return 352;
-        default:
-            return -1;
-        }
+        vector<string> board(n, string(n, '.'));
+        makePosition(board, 0);
+        return ans.size();
     }
 };
