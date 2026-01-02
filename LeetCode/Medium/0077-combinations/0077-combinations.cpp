33f42c4f123f9d0e0e796edaa945d5c8cbddf13f
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-02 15:01:32 +0600

    0077-combinations Medium 01-02-2026

--- LeetCode/Medium/0077-combinations/0077-combinations.cpp
+++ LeetCode/Medium/0077-combinations/0077-combinations.cpp
@@ -1,15 +1,13 @@
 class Solution {
 private:
-    int k, n;
-    vector<vector<int>> ans;
-    void getCombo(int idx, vector<int>& combo) {
+    void getCombo(int idx, int n, int k, vector<int>& combo, vector<vector<int>>& ans) {
         if (combo.size() == k) {
             ans.push_back(combo);
             return;
         }
         for (int i = idx; i <= n; i++) {
             combo.push_back(i);
-            getCombo(i + 1, combo);
+            getCombo(i + 1, n, k, combo, ans);
             combo.pop_back();
         }
     }
@@ -17,9 +15,8 @@ private:
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<int> combo;
-        this->k = k;
-        this->n = n;
-        getCombo(1, combo);
+        vector<vector<int>> ans;
+        getCombo(1, n, k, combo, ans);
         return ans;
     }
 };
