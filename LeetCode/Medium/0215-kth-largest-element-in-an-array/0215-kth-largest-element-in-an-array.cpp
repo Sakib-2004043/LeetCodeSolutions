c0c55928202b3f1fd7043e40fd85a8d4530c6832
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-03 00:13:37 +0600

    0215-kth-largest-element-in-an-array Medium 01-03-2026

--- LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-       sort(nums.rbegin(),nums.rend());
-       return nums[k-1]; 
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (int i = 0; i < nums.size(); i++) {
+            minHeap.push(nums[i]);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
     }
 };
