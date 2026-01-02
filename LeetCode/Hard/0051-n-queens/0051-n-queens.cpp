d0204327ed428802cbdc004033dc7e0e0d4e4880
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-02 21:44:46 +0600

    0051-n-queens Hard 01-02-2026

---
+++ LeetCode/Hard/0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,44 @@
+class Solution {
+private:
+    vector<vector<string>> ans;
+    bool isSafe(vector<string>& board, int row, int col) {
+        int n = board.size();
+        for (int i = 0; i < n; i++) {
+            if (board[row][i] == 'Q' || board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    void makePosition(vector<string>& board, int row) {
+        int n = board.size();
+        if(row == n){
+            ans.push_back(board);
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                makePosition(board, row + 1);
+            }
+            board[row][col] = '.';
+        }
+    }
+
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        makePosition(board, 0);
+        return ans;
+    }
+};
