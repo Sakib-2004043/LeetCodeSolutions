b0e169a6d2214a95954f160a24099fc1011319d4
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-03 14:24:39 +0600

    0010-regular-expression-matching Hard 01-03-2026

--- LeetCode/Hard/0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ LeetCode/Hard/0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
+    vector<vector<int>> memo;
+
     bool solve(int i, int j, string &s, string &p) {
-        // If pattern is finished
+        if (memo[i][j] != -1)
+            return memo[i][j];
+
+        bool ans;
         if (j == p.size()) {
-            return i == s.size();
+            ans = (i == s.size());
+        } else {
+            bool firstMatch = (i < s.size() &&
+                              (s[i] == p[j] || p[j] == '.'));
+
+            if (j + 1 < p.size() && p[j + 1] == '*') {
+                ans = solve(i, j + 2, s, p) ||
+                      (firstMatch && solve(i + 1, j, s, p));
+            } else {
+                ans = firstMatch && solve(i + 1, j + 1, s, p);
+            }
         }
 
-        // Check current character match
-        bool firstMatch = (i < s.size()) &&
-                          (s[i] == p[j] || p[j] == '.');
-
-        // If next char is '*'
-        if (j + 1 < p.size() && p[j + 1] == '*') {
-            // zero occurrence OR one/more occurrence
-            return solve(i, j + 2, s, p) ||
-                   (firstMatch && solve(i + 1, j, s, p));
-        } 
-        // Normal case
-        else {
-            return firstMatch && solve(i + 1, j + 1, s, p);
-        }
+        return memo[i][j] = ans;
     }
 
     bool isMatch(string s, string p) {
+        memo.assign(s.size() + 1, vector<int>(p.size() + 1, -1));
         return solve(0, 0, s, p);
     }
 };
