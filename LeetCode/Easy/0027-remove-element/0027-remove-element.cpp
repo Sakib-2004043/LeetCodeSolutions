545e441bf8e00106cc29d041d97f15a7f9adc314
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-03 14:19:17 +0600

    0027-remove-element Easy 01-03-2026

--- LeetCode/Easy/0027-remove-element/0027-remove-element.cpp
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.cpp
@@ -1,16 +1,12 @@
-#include<bits/stdc++.h>
-typedef long long lli;
-using namespace std;
-
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        lli n,i,k=0;
-        n=nums.size();
+        long long n, i, k = 0;
+        n = nums.size();
 
-        for(i=0;i<n;i++){
-            if(nums[i]!=val){
-                nums[k]=nums[i];
+        for (i = 0; i < n; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
                 k++;
             }
         }
