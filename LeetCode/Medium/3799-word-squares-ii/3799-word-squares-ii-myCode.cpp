c66f65b19c60a900397c56c79db863a160a4c21f
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-04 17:59:28 +0600

    3799-word-squares-ii Medium 01-04-2026

---
+++ LeetCode/Medium/3799-word-squares-ii/3799-word-squares-ii-myCode.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    set<vector<string>> ans;
+    vector<string> words;
+    unordered_map<string, bool> visited;
+    void makeSquare(vector<string>& square) {
+        if (square.size() == 2) {
+            if (square[0][0] != square[1][0]) {
+                return;
+            }
+        } else if (square.size() == 3) {
+            if (square[0][3] != square[2][0]) {
+                return;
+            }
+        } else if (square.size() == 4) {
+            if (square[3][0] == square[1][3] && square[3][3] == square[2][3]) {
+                ans.insert(square);
+            }
+            return;
+        }
+        for (int i = 0; i < words.size(); i++) {
+            if (visited[words[i]] == false) {
+                square.push_back(words[i]);
+                visited[words[i]] = true;
+                makeSquare(square);
+                square.pop_back();
+                visited[words[i]] = false;
+            }  
+        }
+    }
+
+public:
+    vector<vector<string>> wordSquares(vector<string>& words) {
+        this->words = words;
+        vector<string> square;
+        makeSquare(square);
+        return vector<vector<string>>(ans.begin(), ans.end());
+    }
+};
