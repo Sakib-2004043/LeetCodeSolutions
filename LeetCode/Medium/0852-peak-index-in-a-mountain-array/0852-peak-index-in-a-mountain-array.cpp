7e3f213cb4cb8b12a3bbbab470095b2414f8e064
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-05 01:10:12 +0600

    0852-peak-index-in-a-mountain-array Medium 01-05-2026

---
+++ LeetCode/Medium/0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0;
+        int right = arr.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            cout << left << " " << mid << " " << right << endl;
+            if (mid > 0 && arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return 0;
+    }
+};
