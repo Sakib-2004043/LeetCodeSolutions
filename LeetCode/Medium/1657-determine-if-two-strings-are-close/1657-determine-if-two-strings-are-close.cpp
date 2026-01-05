b1f7012b42db1b73c881087a234882bcc5a73925
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-06 00:50:13 +0600

    1657-determine-if-two-strings-are-close Medium 01-06-2026

--- LeetCode/Medium/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ LeetCode/Medium/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size()!=word2.size()){
+        if (word1.size() != word2.size()) {
             return false;
         }
         unordered_map<char, int> firstWord, secondWord;
-        vector<int>vec1,vec2;
-        for(int i=0;i<word1.size();i++){
+        vector<int> vec1, vec2;
+        for (int i = 0; i < word1.size(); i++) {
             firstWord[word1[i]]++;
             secondWord[word2[i]]++;
         }
-        for(auto x:firstWord){
-            if(!secondWord.count(x.first)){
+        if (firstWord.size() != secondWord.size()) {
+            return false;
+        }
+        for (auto x : firstWord) {
+            if (!secondWord.count(x.first)) {
                 return false;
             }
             vec1.push_back(x.second);
         }
-        for(auto x:secondWord){
-            if(!firstWord.count(x.first)){
+        for (auto x : secondWord) {
+            if (!firstWord.count(x.first)) {
                 return false;
             }
             vec2.push_back(x.second);
         }
-        sort(vec1.begin(),vec1.end());
-        sort(vec2.begin(),vec2.end());
+        sort(vec1.begin(), vec1.end());
+        sort(vec2.begin(), vec2.end());
         return vec1 == vec2;
     }
 };
