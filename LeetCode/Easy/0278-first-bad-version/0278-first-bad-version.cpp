fa670b341a8c8c81ac376c154a14ac7afb519214
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-05 20:10:39 +0600

    0278-first-bad-version Easy 01-05-2026

--- LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.cpp
+++ LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,22 +4,21 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        if(n==1){
+        if (n == 1) {
             return 1;
         }
         int left = 1;
         int right = n;
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if (isBadVersion(mid) != isBadVersion(mid + 1)) {
-                return mid + 1;
-            }
-            if (isBadVersion(mid) && isBadVersion(mid + 1)) {
-                right = mid - 1;
+            if (isBadVersion(mid)) {
+                right = mid;
             } else {
                 left = mid + 1;
             }
         }
+        return left;
+
         return 1;
     }
 };
