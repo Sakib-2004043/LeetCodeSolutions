bf5b84536da8190ac72edc4d022c9fb3a582ae06
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-10 23:42:03 +0600

    1926-nearest-exit-from-entrance-in-maze Medium 01-10-2026

--- LeetCode/Medium/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ LeetCode/Medium/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int row, col, steps, ans;
-        queue<vector<int>> cells;
-        row = maze.size();
-        col = maze[0].size();
+        int row = maze.size();
+        int col = maze[0].size();
+
         vector<vector<bool>> visited(row, vector<bool>(col, false));
-        cells.push(entrance);
+        queue<pair<int, int>> q;
+
+        q.push({entrance[0], entrance[1]});
         visited[entrance[0]][entrance[1]] = true;
-        steps = 0, ans = -1;
-        while (!cells.empty()) {
-            int sz = cells.size();
+
+        int steps = 0;
+
+        while (!q.empty()) {
+            int sz = q.size();
             while (sz--) {
-                int x = cells.front()[0];
-                int y = cells.front()[1];
-                if (cells.front() != entrance &&
+                auto [x, y] = q.front();
+                q.pop();
+
+                if (!(x == entrance[0] && y == entrance[1]) &&
                     (x == 0 || y == 0 || x == row - 1 || y == col - 1)) {
-                    ans = steps;
-                    goto last;
+                    return steps;
                 }
+
                 if (x > 0 && maze[x - 1][y] == '.' && !visited[x - 1][y]) {
-                    cells.push({x - 1, y});
                     visited[x - 1][y] = true;
+                    q.push({x - 1, y});
                 }
                 if (y > 0 && maze[x][y - 1] == '.' && !visited[x][y - 1]) {
-                    cells.push({x, y - 1});
                     visited[x][y - 1] = true;
+                    q.push({x, y - 1});
                 }
                 if (x + 1 < row && maze[x + 1][y] == '.' &&
                     !visited[x + 1][y]) {
-                    cells.push({x + 1, y});
                     visited[x + 1][y] = true;
+                    q.push({x + 1, y});
                 }
                 if (y + 1 < col && maze[x][y + 1] == '.' &&
                     !visited[x][y + 1]) {
-                    cells.push({x, y + 1});
                     visited[x][y + 1] = true;
+                    q.push({x, y + 1});
                 }
-                cells.pop();
             }
             steps++;
         }
-    last:;
-        return ans;
+        return -1;
     }
 };
