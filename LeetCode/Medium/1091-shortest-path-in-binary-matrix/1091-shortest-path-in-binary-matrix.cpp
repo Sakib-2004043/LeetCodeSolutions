2a9c7d5996c5dd78d9ad8dfc1b02e3394393fc9e
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-12 21:25:58 +0600

    1091-shortest-path-in-binary-matrix Medium 01-12-2026

---
+++ LeetCode/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        if (grid[0][0] == 1) {
+            return -1;
+        }
+        int n = grid.size();
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0},  {-1, 0},
+                                          {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        queue<pair<int, int>> bfsQ;
+        bfsQ.push({0, 0});
+        int steps = 1;
+        while (!bfsQ.empty()) {
+            int sz = bfsQ.size();
+            while (sz--) {
+                auto [x, y] = bfsQ.front();
+                bfsQ.pop();
+                if (x + 1 == n && y + 1 == n && grid[x][y] == 0) {
+                    return steps;
+                }
+                for (auto& dir : directions) {
+                    int nr = x + dir[0];
+                    int nc = y + dir[1];
+                    if (nc >= 0 && nr >= 0 && nc < n && nr < n &&
+                        grid[nr][nc] == 0 && !visited[nr][nc]) {
+                        bfsQ.push({nr, nc});
+                        visited[nr][nc] = true;
+                    }
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
