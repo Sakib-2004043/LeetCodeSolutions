42b74c910cc216782823dd211fde5ae6a2342738
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-13 23:02:11 +0600

    1137-n-th-tribonacci-number Easy 01-13-2026

--- LeetCode/Easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ LeetCode/Easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -3,11 +3,9 @@ private:
     vector<int> dp;
 
 public:
-    Solution(){
-        dp.assign(40,0);
-    }
+    Solution() { dp.assign(40, -1); }
     int tribonacci(int n) {
-        if (dp[n] != 0) {
+        if (dp[n] != -1) {
             return dp[n];
         }
         if (n == 0 || n == 1 || n == 2) {
