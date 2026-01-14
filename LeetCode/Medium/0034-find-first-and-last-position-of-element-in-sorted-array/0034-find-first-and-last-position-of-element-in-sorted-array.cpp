0572442dafcc760a0ae29c2cfd9c98ee38986156
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-14 18:26:09 +0600

    0034-find-first-and-last-position-of-element-in-sorted-array Medium 01-14-2026

--- LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -9,12 +9,11 @@ public:
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] >= target)
-                right = mid - 1;
-            else
+            if (nums[mid] < target)
                 left = mid + 1;
+            else
+                right = mid - 1;
         }
-
         if (left == n || nums[left] != target)
             return ans;
 
@@ -24,12 +23,11 @@ public:
         right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] <= target)
-                left = mid + 1;
-            else
+            if (nums[mid] > target)
                 right = mid - 1;
+            else
+                left = mid + 1;
         }
-
         ans[1] = right;
         return ans;
     }
