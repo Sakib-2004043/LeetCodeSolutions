b06b31651033be2cff4eff83e91c5532667b2883
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-16 16:09:50 +0600

    1934-confirmation-rate Medium 01-16-2026

--- LeetCode/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
+++ LeetCode/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,8 +1,23 @@
 # Write your MySQL query statement below
-select 
-    s.user_id, 
-    round(avg(case when c.action = 'confirmed' then 1 else 0 end),2) as confirmation_rate
-from Signups as s
-left join Confirmations as c
-on s.user_id = c.user_id
-group by s.user_id
+-- select 
+--     s.user_id, 
+--     round(
+--         avg(case when c.action = 'confirmed' then 1 else 0 end)
+--         ,2
+--     ) as confirmation_rate
+-- from Signups as s
+-- left join Confirmations as c
+-- on s.user_id = c.user_id
+-- group by s.user_id
+
+SELECT 
+    s.user_id,
+    ROUND(
+        IFNULL(SUM(c.action = 'confirmed') / NULLIF(COUNT(c.action), 0), 0),
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
+
