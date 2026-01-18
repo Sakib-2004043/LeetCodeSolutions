ead3bca6bdcfc1c8dc236f037098f454d6b20252
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-18 18:41:29 +0600

    0001-two-sum Easy 01-18-2026

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> mp;  // value -> index
-
         for (int i = 0; i < nums.size(); i++) {
             int find = target - nums[i];
             if (mp.count(find)) {
@@ -10,7 +9,6 @@ public:
             }
             mp[nums[i]] = i;
         }
-
         return {};
     }
 };
