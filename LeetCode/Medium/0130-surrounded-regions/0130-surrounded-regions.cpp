39c1acef2017e5d06b92b5470e8c09fa8e157176
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-20 14:47:17 +0600

    0130-surrounded-regions Medium 01-20-2026

---
+++ LeetCode/Medium/0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    vector<vector<bool>> visited;
+    void check_dfs(auto& board, int row, int col) {
+        if (visited[row][col]) {
+            return;
+        }
+        visited[row][col] = true;
+        int totalRow = board.size();
+        int totalCol = board[0].size();
+        vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (auto& dir : directions) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            if (newRow >= 0 && newRow < totalRow && newCol >= 0 &&
+                newCol < totalCol && !visited[newRow][newCol] &&
+                board[newRow][newCol] == 'O') {
+                check_dfs(board, newRow, newCol);
+            }
+        }
+    }
+
+public:
+    void solve(vector<vector<char>>& board) {
+        int row = board.size();
+        int col = board[0].size();
+        visited.assign(row, vector<bool>(col, false));
+        for (int i = 0; i < row; i++) {
+            int j = 0;
+            while (j < col) {
+                if (board[i][j] == 'O') {
+                    check_dfs(board, i, j);
+                }
+                j += (i == 0 || i == row - 1) ? 1 : (col - 1);
+            }
+        }
+        for (int i = 1; i < row - 1; i++) {
+            for (int j = 1; j < col - 1; j++) {
+                if (board[i][j] == 'O' && !visited[i][j]) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
