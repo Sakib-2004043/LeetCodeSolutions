4ed680a6644048e9a2a2262256425861903591a5
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-23 15:52:22 +0600

    0279-perfect-squares Medium 01-23-2026

---
+++ LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> prime;
+        for (int i = 1; i * i <= n; i++) {
+            prime.push_back(i * i);
+        }
+        for (auto x : prime) {
+            cout << x << " ";
+        }
+        vector<int> dp(n + 5, 0);
+        for (int i = 1; i <= n; i++) {
+            if ((int)sqrt(i) * (int)sqrt(i) == i) {
+                dp[i] = 1;
+                continue;
+            }
+            int mn = INT_MAX;
+            for (int j = 0; j < prime.size() && prime[j] < i; j++) {
+                mn = min(mn, dp[prime[j]] + dp[i - prime[j]]);
+            }
+            dp[i] = mn;
+        }
+        return dp[n];
+    }
+};
