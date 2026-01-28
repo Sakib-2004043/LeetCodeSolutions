6d5e4fe493f1b65d9597cd15e09f8291272f1f11
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-28 21:42:07 +0600

    0096-unique-binary-search-trees Medium 01-28-2026

--- LeetCode/Medium/0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ LeetCode/Medium/0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,15 +1,15 @@
-// Catalan number Cₙ.
-// ans = (2n)!/(n+1)/((n!)^2)
+// Pure DP
 class Solution {
 public:
     int numTrees(int n) {
-        double ans = 1.0 / (n + 1);
-        for (int i = 1; i <= 2 * n; i++) {
-            ans *= i;
-            if (i <= n) {
-                ans /= i * i;
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int nodes = 2; nodes <= n; nodes++) {
+            for (int root = 1; root <= nodes; root++) {
+                dp[nodes] += dp[root - 1] * dp[nodes - root];
             }
         }
-        return round(ans);
+        return dp[n];
     }
 };
