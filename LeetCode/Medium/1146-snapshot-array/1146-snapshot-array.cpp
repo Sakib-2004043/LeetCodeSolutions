2329a665141db6e0d7fa53f99498994d89faa420
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-29 23:56:45 +0600

    1146-snapshot-array Medium 01-29-2026

---
+++ LeetCode/Medium/1146-snapshot-array/1146-snapshot-array.cpp
@@ -0,0 +1,28 @@
+class SnapshotArray {
+private:
+    int snap_id;
+    map<int, map<int, int>> mp;
+
+public:
+    SnapshotArray(int length) { snap_id = length * 0; }
+
+    void set(int index, int val) { mp[index][snap_id] = val; }
+
+    int snap() {
+        snap_id++;
+        return snap_id - 1;
+    }
+
+    int get(int index, int snap_id) {
+        auto ub = mp[index].upper_bound(snap_id);
+        return prev(ub)->second;
+    }
+};
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray* obj = new SnapshotArray(length);
+ * obj->set(index,val);
+ * int param_2 = obj->snap();
+ * int param_3 = obj->get(index,snap_id);
+ */
