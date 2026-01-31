46dcb8ca38c935ee72d32e317f066ef1a3193425
Sakibul Hasan <sakib1514817122@gmail.com>
2026-01-31 16:29:08 +0600

    0121-best-time-to-buy-and-sell-stock Easy 01-31-2026

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int mx, mn, n, ans;
+        int i, mx, mn, n, ans;
         n = prices.size();
         mx = prices[0];
         mn = prices[0];
         ans = 0;
-        for (int i = 1; i < n; i++) {
+        for (i = 1; i < n; i++) {
             if (mn > prices[i]) {
                 mn = prices[i];
                 mx = 0;
