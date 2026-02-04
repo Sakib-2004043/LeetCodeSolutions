97817acea939ead476590fa3721328f466b912e9
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-04 16:27:44 +0600

    0139-word-break Medium 02-04-2026

--- LeetCode/Medium/0139-word-break/0139-word-break.cpp
+++ LeetCode/Medium/0139-word-break/0139-word-break.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wd) {
-        // int maxLen = 0;
-        // for (auto& word : wd) {
-        //     maxLen = max(maxLen, (int)word.size());
-        // }
+        int maxLen = 0;
+        for (auto& word : wd) {
+            maxLen = max(maxLen, (int)word.size());
+        }
         int n = s.size();
         sort(wd.begin(), wd.end());
         vector<bool> dp(n + 1, false);
         dp[0] = true;
         for (int i = 1; i <= n; i++) {
-            for (int j = i - 1; j >= 0 ; j--) {
+            for (int j = i - 1; j >= 0 && (i - j) <= maxLen; j--) {
                 string subString = s.substr(j, i - j);
                 bool found = binary_search(wd.begin(), wd.end(), subString);
                 if (found && dp[j]) {
