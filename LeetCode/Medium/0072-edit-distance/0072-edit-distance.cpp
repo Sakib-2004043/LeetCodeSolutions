f87740d28bc1a57fe22f67accba15bc267753b4d
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-05 13:35:49 +0600

    0072-edit-distance Medium 02-05-2026

---
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int row = word1.size();
+        int col = word2.size();
+
+        vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
+
+        // base cases
+        for (int i = 0; i <= row; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= col; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+                }
+            }
+        }
+        return dp[row][col];
+    }
+};
