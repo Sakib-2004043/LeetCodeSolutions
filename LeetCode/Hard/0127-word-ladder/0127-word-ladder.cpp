11fa8cfba33736ab0a12892579d052a68e89e08e
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-05 23:19:30 +0600

    0127-word-ladder Hard 02-05-2026

--- LeetCode/Hard/0127-word-ladder/0127-word-ladder.cpp
+++ LeetCode/Hard/0127-word-ladder/0127-word-ladder.cpp
@@ -2,34 +2,38 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord,
                      vector<string>& wordList) {
-        int ans = 0;
-        unordered_map<string, bool> visited;
-        queue<string> bfsQ;
-        bfsQ.push(beginWord);
-        sort(wordList.begin(), wordList.end());
-        while (!bfsQ.empty()) {
-            int sz = bfsQ.size();
-            ans++;
-            while (sz--) {
-                string currWord = bfsQ.front();
-                bfsQ.pop();
-                if (currWord == endWord) {
-                    return ans;
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        if (!wordSet.count(endWord)) {
+            return 0;
+        }
+        unordered_set<string> visited;
+        queue<string> q;
+        q.push(beginWord);
+
+        int steps = 1;
+        while (!q.empty()) {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                string word = q.front();
+                q.pop();
+                if (word == endWord) {
+                    return steps;
                 }
-                for (int i = 0; i < currWord.size(); i++) {
-                    string tempWord = currWord;
+                for (int j = 0; j < word.size(); j++) {
+                    char originalChar = word[j];
                     for (char c = 'a'; c <= 'z'; c++) {
-                        tempWord[i] = c;
-                        bool found = binary_search(wordList.begin(),
-                                                   wordList.end(), tempWord);
-                        if (found && !visited[tempWord]) {
-                            bfsQ.push(tempWord);
-                            visited[tempWord] = true;
+                        word[j] = c;
+                        if (wordSet.count(word) && !visited.count(word)) {
+                            q.push(word);
+                            visited.insert(word);
                         }
                     }
+                    word[j] = originalChar;
                 }
             }
+            steps++;
         }
+
         return 0;
     }
 };
