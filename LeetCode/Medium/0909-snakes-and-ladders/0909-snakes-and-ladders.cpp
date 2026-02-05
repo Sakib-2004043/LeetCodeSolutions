4388d7c59c4b533a89d5a40324259ba8433f7419
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-05 19:56:57 +0600

    0909-snakes-and-ladders Medium 02-05-2026

---
+++ LeetCode/Medium/0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        queue<int> bfsQ;
+        int n = board.size();
+        int ans = 0;
+        reverse(board.begin(), board.end());
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 1) {
+                reverse(board[i].begin(), board[i].end());
+            }
+        }
+        // for (auto v : board) {
+        //     for (auto x : v) {
+        //         cout << x << " ";
+        //     }
+        //     cout << endl;
+        // }
+        vector<int> jump(n * n + 1, 0);
+        for (int i = 0, k = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                jump[k] = (board[i][j] != -1) ? board[i][j] : k;
+                k++;
+            }
+        }
+        vector<bool> visited(n * n + 5, false);
+        bfsQ.push(jump[1]);
+        while (!bfsQ.empty()) {
+            int sz = bfsQ.size();
+            ans++;
+            while (sz--) {
+                int num = bfsQ.front();
+                bfsQ.pop();
+                for (int i = num + 1; i <= num + 6; i++) {
+                    int nextCell = jump[i];
+                    if (nextCell == n * n) {
+                        return ans;
+                    }
+                    if (nextCell < n * n && !visited[nextCell]) {
+                        bfsQ.push(nextCell);
+                        visited[nextCell] = true;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
