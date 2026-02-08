7944cf43bfb72b4d8ae67c8d951d8a427f1df1a8
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-08 18:34:52 +0600

    0212-word-search-ii Hard 02-08-2026

--- LeetCode/Hard/0212-word-search-ii/0212-word-search-ii.cpp
+++ LeetCode/Hard/0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,69 +1,52 @@
 class Solution {
 private:
+    vector<vector<bool>> visited;
+    unordered_set<string> ans;
+    unordered_set<string> wordSet;
+    unordered_set<string> prefixSet;
+
+    vector<vector<int>> directions{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
     void backTrackDFS(vector<vector<char>>& board, int row, int col,
-                      unordered_set<string>& ans,
-                      unordered_set<string>& wordSet,
-                      unordered_set<string>& prefixSet,
-                      vector<vector<bool>>& visited,
-                      vector<vector<int>>& directions, string& curWord) {
-
-        if (prefixSet.count(curWord) == 0)
+                      string& curWord) {
+        if (prefixSet.count(curWord) == 0) {
             return;
-
+        }
         if (wordSet.count(curWord)) {
             ans.insert(curWord);
         }
-
         visited[row][col] = true;
+        for (auto& d : directions) {
+            int nr = row + d[0], nc = col + d[1];
+            if (nr >= 0 && nc >= 0 && nr < board.size() &&
+                nc < board[0].size() && !visited[nr][nc]) {
 
-        int totalRow = board.size();
-        int totalCol = board[0].size();
-
-        for (auto& dir : directions) {
-            int newRow = row + dir[0];
-            int newCol = col + dir[1];
-
-            if (newRow >= 0 && newCol >= 0 && newRow < totalRow &&
-                newCol < totalCol && !visited[newRow][newCol]) {
-
-                curWord.push_back(board[newRow][newCol]);
-                backTrackDFS(board, newRow, newCol, ans, wordSet, prefixSet,
-                             visited, directions, curWord);
-                curWord.pop_back(); // 🔥 critical
+                curWord.push_back(board[nr][nc]);
+                backTrackDFS(board, nr, nc, curWord);
+                curWord.pop_back();
             }
         }
-
         visited[row][col] = false;
     }
 
 public:
     vector<string> findWords(vector<vector<char>>& board,
                              vector<string>& words) {
-
-        int row = board.size();
-        int col = board[0].size();
-
-        unordered_set<string> ans, wordSet, prefixSet;
-        vector<vector<bool>> visited(row, vector<bool>(col, false));
-        vector<vector<int>> directions{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
+        int r = board.size(), c = board[0].size();
         for (string& w : words) {
             wordSet.insert(w);
             for (int i = 1; i <= w.size(); i++) {
                 prefixSet.insert(w.substr(0, i));
             }
         }
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                string curWord;
-                curWord.push_back(board[i][j]);
-
-                backTrackDFS(board, i, j, ans, wordSet, prefixSet, visited,
-                             directions, curWord);
+        visited.assign(r, vector<bool>(c, false));
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                string cur;
+                cur.push_back(board[i][j]);
+                backTrackDFS(board, i, j, cur);
             }
         }
-
         return vector<string>(ans.begin(), ans.end());
     }
 };
