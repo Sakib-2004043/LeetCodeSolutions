9a2815df03b20330d2a85070c558210d3be0ff71
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-09 23:34:13 +0600

    0373-find-k-pairs-with-smallest-sums Medium 02-09-2026

---
+++ LeetCode/Medium/0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    using T = tuple<int, int, int>;
+    priority_queue<T, vector<T>, greater<T>> pq;
+
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2,
+                                       int k) {
+        vector<vector<int>> ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            pq.push({nums1[i] + nums2[0], i, 0});
+        }
+        while (k--) {
+            auto [sum, i, j] = pq.top();
+            pq.pop();
+            ans.push_back({nums1[i], nums2[j]});
+            if (j + 1 < nums2.size()) {
+                pq.push({nums1[i] + nums2[j + 1], i, j + 1});
+            }
+        }
+        return ans;
+    }
+};
