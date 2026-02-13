b5ee06a5d49f5d317c0cccf25c68bf58c4a9df19
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-14 00:22:53 +0600

    0003-longest-substring-without-repeating-characters Medium 02-14-2026

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int i, j, n, mx;
-        unordered_map<char, bool> visited;
-        n = s.size();
-        if(n==1){
+        vector<bool> visited(200);
+        int n = s.size();
+        if (n == 1) {
             return 1;
         }
-        mx = 0;
-        i = 0;
-        j = 0;
+        int mx = 0, i = 0, j = 0;
         while (i < n) {
             if (!visited[s[i]]) {
                 visited[s[i]] = true;
