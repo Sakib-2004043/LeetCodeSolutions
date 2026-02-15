6ffca10eae7b60db75224480b4fc5ea5d1782421
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-15 15:04:12 +0600

    0123-best-time-to-buy-and-sell-stock-iii Hard 02-15-2026

---
+++ LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    vector<vector<vector<int>>> dp;
+    int calcDFS(vector<int>& prices, int idx, int canBuy, int count) {
+        if (count == 0 || idx == prices.size()) {
+            return 0;
+        }
+        if (dp[idx][canBuy][count] != -1) {
+            return dp[idx][canBuy][count];
+        }
+        if (canBuy == 1) {
+            int canBuySTC = calcDFS(prices, idx + 1, 0, count) - prices[idx];
+            int notcanBuy = calcDFS(prices, idx + 1, 1, count);
+            return dp[idx][canBuy][count] = max(canBuySTC, notcanBuy);
+        }
+        int notSell = calcDFS(prices, idx + 1, 0, count);
+        int sellSTC = calcDFS(prices, idx + 1, 1, count - 1) + prices[idx];
+        return dp[idx][canBuy][count] = max(sellSTC, notSell);
+    }
+
+public:
+    int maxProfit(vector<int>& prices) {
+        int k = 2;
+        dp.assign(prices.size(),vector<vector<int>>(2, vector<int>(k + 1, -1)));
+        int ans = calcDFS(prices, 0, 1, k);
+        return ans;
+    }
+};
