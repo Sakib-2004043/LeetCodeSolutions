6b183bebdfca36b3071d7b1caac0f66cba6a2c9c
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-26 23:18:47 +0600

    0790-domino-and-tromino-tiling Medium 02-26-2026

--- LeetCode/Medium/0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ LeetCode/Medium/0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -5,7 +5,9 @@ public:
         vector<int> dp(n + 1);
         dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
+        if (n >= 2) {
+            dp[2] = 2;
+        }
         for (int i = 3; i <= n; i++) {
             dp[i] = ((dp[i - 1] * 2) % MOD + dp[i - 3]) % MOD;
         }
