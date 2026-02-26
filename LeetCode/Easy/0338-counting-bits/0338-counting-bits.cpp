f33657f9c7d1076f9250ffee44d16ac31c4ccdc3
Sakibul Hasan <sakib1514817122@gmail.com>
2026-02-26 22:42:12 +0600

    0338-counting-bits Easy 02-26-2026

--- LeetCode/Easy/0338-counting-bits/0338-counting-bits.cpp
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
+    int popCount(int num) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            count += num >> i & 1;
+        }
+        return count;
+    }
     vector<int> countBits(int n) {
-        vector<int>ans;
-        for(int i = 0; i <= n; i++){
-            ans.push_back(__builtin_popcount(i));
+        vector<int> ans;
+        for (int i = 0; i <= n; i++) {
+            ans.push_back(popCount(i));
         }
         return ans;
     }
