df0b78b9f6350c61682be055cbeabda47946256d
Sakibul Hasan <sakib1514817122@gmail.com>
2026-03-03 16:31:56 +0600

    1071-greatest-common-divisor-of-strings Easy 03-03-2026

--- LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,28 +1,10 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        string ans, sub = "";
-        int n = str1.size();
-        for (int i = 0; i < n; i++) {
-            sub += str1[i];
-            int pos1 = 0, count1 = 0;
-            while ((pos1 = str1.find(sub, pos1)) != string::npos) {
-                count1++;
-                pos1 += sub.length();
-            }
-            if (count1 * sub.size() != str1.size()) {
-                continue;
-            }
-            int pos2 = 0, count2 = 0;
-            while ((pos2 = str2.find(sub, pos2)) != string::npos) {
-                count2++;
-                pos2 += sub.length();
-            }
-            if (count2 * sub.size() != str2.size()) {
-                continue;
-            }
-            ans = sub;
+        if (str1 + str2 != str2 + str1) {
+            return "";
         }
-        return ans;
+        int len = gcd(str1.size(), str2.size());
+        return str1.substr(0, len);
     }
 };
