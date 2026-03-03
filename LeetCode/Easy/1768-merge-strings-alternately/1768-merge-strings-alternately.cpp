da98f35980265af42f7e48257debca9e5805ef37
Sakibul Hasan <sakib1514817122@gmail.com>
2026-03-03 15:27:49 +0600

    1768-merge-strings-alternately Easy 03-03-2026

---
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1 = word1.size();
+        int n2 = word2.size();
+        string ans = "";
+        int i = 0, j = 0;
+        while (i < n1 && j < n2) {
+            ans += word1[i++];
+            ans += word2[j++];
+        }
+        while (i < n1){
+            ans += word1[i++];
+        }
+        while (j < n2) {
+            ans += word2[j++];
+        }
+        return ans;
+    }
+};
