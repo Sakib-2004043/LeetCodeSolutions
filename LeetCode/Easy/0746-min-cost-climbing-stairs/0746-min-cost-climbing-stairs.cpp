c729a0e3b59ff5cbc7b8288ab327a1b643ca7a7f
Sakibul Hasan <sakib1514817122@gmail.com>
2026-03-04 22:05:52 +0600

    0746-min-cost-climbing-stairs Easy 03-04-2026

--- LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n + 1);
+        vector<int> dp(n);
         dp[0] = cost[0];
         dp[1] = cost[1];
         int ans = 0;
