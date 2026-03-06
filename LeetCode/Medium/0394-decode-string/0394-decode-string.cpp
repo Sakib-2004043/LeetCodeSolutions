fac908a8e2183c2592e4319898d60a4e595f66a1
Sakibul Hasan <sakib1514817122@gmail.com>
2026-03-06 16:17:34 +0600

    0394-decode-string Medium 03-06-2026

---
+++ LeetCode/Medium/0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    string dfs(string s, int& idx) {
+        string ans = "";
+        while (idx < s.size()) {
+            if (s[idx] == '[') {
+                idx++;
+                continue;
+            }
+            if (s[idx] == ']') {
+                idx++;
+                return ans;
+            }
+            if (isalpha(s[idx])) {
+                ans += s[idx++];
+                continue;
+            }
+            int digit = 0;
+            while (isdigit(s[idx])) {
+                digit *= 10;
+                digit += (s[idx] - '0');
+                idx++;
+            }
+            string temp = dfs(s, idx);
+            for (int j = 1; j <= digit; j++) {
+                ans += temp;
+            }
+        }
+        return ans;
+    }
+
+public:
+    string decodeString(string s) {
+        int i = 0;
+        return dfs(s, i);
+    }
+};
