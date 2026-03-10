1db7daaabad7984d9d6e3816ba4b8964f4bb29c9
Sakibul Hasan <sakib1514817122@gmail.com>
2026-03-11 01:00:11 +0600

    0131-palindrome-partitioning Medium 03-11-2026

--- LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,26 +1,31 @@
 class Solution {
+private:
     vector<vector<string>> ans;
-    void backTrack(string& s, vector<string>& vs, int idx) {
-        if (idx == s.size()) {
-            ans.push_back(vs);
+    bool isPal(string& s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) {
+                return false;
+            }
         }
-        int n = s.size();
-        for (int i = idx; i < n; i++) {
-            string as = s.substr(idx, i - idx + 1);
-            string cs = as;
-            reverse(cs.begin(), cs.end());
-            if (as == cs) {
-                vs.push_back(as);
-                backTrack(s, vs, i + 1);
-                vs.pop_back();
+        return true;
+    }
+    void backTrack(string& s, vector<string>& cur, int idx) {
+        if (idx == s.size()) {
+            ans.push_back(cur);
+        }
+        for (int i = idx; i < s.size(); i++) {
+            if (isPal(s, idx, i)) {
+                cur.push_back(s.substr(idx, i - idx + 1));
+                backTrack(s, cur, i + 1);
+                cur.pop_back();
             }
         }
     }
 
 public:
     vector<vector<string>> partition(string s) {
-        vector<string> vs;
-        backTrack(s, vs, 0);
+        vector<string> cur;
+        backTrack(s, cur, 0);
         return ans;
     }
 };
