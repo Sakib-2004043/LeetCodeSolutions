3445c26fff75a12f2f51e45c2e9db681a05f3d47
Sakibul Hasan <sakib1514817122@gmail.com>
2026-03-10 17:15:26 +0600

    0435-non-overlapping-intervals Medium 03-10-2026

---
+++ LeetCode/Medium/0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    static bool cmp2(const vector<int>& a, const vector<int>& b) {
+        // if (a[1] == b[1]) {
+        //     return a[0] < b[0];
+        // }
+        return a[1] < b[1];
+    }
+
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), cmp2);
+        for (auto x : intervals) {
+            cout << x[0] << " " << x[1] << endl;
+        }
+        int remove = 0;
+        int preEnd = intervals[0][1];
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] < preEnd) {
+                remove++;
+            } else {
+                preEnd = intervals[i][1];
+            }
+        }
+        return remove;
+    }
+};
