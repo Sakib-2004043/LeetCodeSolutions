d87fc81fe00127924442347addb56370585e0b63
Sakibul Hasan <sakib1514817122@gmail.com>
2026-03-18 23:35:01 +0600

    0287-find-the-duplicate-number Medium 03-18-2026

--- LeetCode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ LeetCode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -17,22 +17,3 @@ public:
         return slow;
     }
 };
-// int findDuplicate(vector<int>& nums) {
-//     int slow = nums[0];
-//     int fast = nums[0];
-
-//     // Phase 1: Detect cycle
-//     do {
-//         slow = nums[slow];
-//         fast = nums[nums[fast]];
-//     } while (slow != fast);
-
-//     // Phase 2: Find cycle entrance
-//     slow = nums[0];
-//     while (slow != fast) {
-//         slow = nums[slow];
-//         fast = nums[fast];
-//     }
-
-//     return slow;
-// }
