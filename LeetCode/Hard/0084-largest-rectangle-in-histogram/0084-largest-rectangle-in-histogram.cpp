fa3f5ae1705372bc6bc7e848b98d6a9eda6f9a6e
Sakibul Hasan <sakib1514817122@gmail.com>
2026-03-28 19:36:02 +0600

    0084-largest-rectangle-in-histogram Hard 03-28-2026

---
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    vector<int> makeArr(auto& ht, int bgn, int end, int it) {
+        stack<int> st;
+        int n = ht.size();
+        vector<int> sn(n, 0);
+        for (int i = bgn; i != end; i += it) {
+            while (!st.empty() && ht[st.top()] >= ht[i]) {
+                st.pop();
+            }
+            sn[i] = !st.empty() ? st.top() : (it == 1 ? -1 : n);
+            st.push(i);
+        }
+        return sn;
+    }
+
+    void display(vector<int>& ns) {
+        for (auto x : ns) cout << x << " ";
+        cout << endl;
+    }
+
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+
+        vector<int> lsn = makeArr(heights, 0, n, 1);
+        display(lsn);
+
+        vector<int> rsn = makeArr(heights, n - 1, -1, -1);
+        display(rsn);
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int area = heights[i] * (rsn[i] - lsn[i] - 1);
+            ans = max(ans, area);
+        }
+        return ans;
+    }
+};
