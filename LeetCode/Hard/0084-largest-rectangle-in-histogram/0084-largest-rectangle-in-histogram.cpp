7b49533135f91d5d40fac338e510eff218f4b0a8
Sakibul Hasan <sakib1514817122@gmail.com>
2026-03-28 19:44:48 +0600

    0084-largest-rectangle-in-histogram Hard 03-28-2026

--- LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,29 +1,20 @@
 class Solution {
-private:
-    vector<int> makeArr(vector<int>& ht, int bgn, int end, int it) {
-        stack<int> st;
-        int n = ht.size();
-        vector<int> sn(n, 0);
-        for (int i = bgn; i != end; i += it) {
-            while (!st.empty() && ht[st.top()] >= ht[i]) {
-                st.pop();
-            }
-            sn[i] = !st.empty() ? st.top() : (it == 1 ? -1 : n);
-            st.push(i);
-        }
-        return sn;
-    }
-
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> lsn = makeArr(heights, 0, n, 1);
-        vector<int> rsn = makeArr(heights, n - 1, -1, -1);
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int area = heights[i] * (rsn[i] - lsn[i] - 1);
-            ans = max(ans, area);
+    int largestRectangleArea(vector<int>& h) {
+    stack<int> st;
+    int n = h.size(), ans = 0;
+    for (int i = 0; i <= n; i++) {
+        int cur = (i == n ? 0 : h[i]);
+        while (!st.empty() && h[st.top()] >= cur) {
+            int height = h[st.top()];
+            st.pop();
+            int right = i;
+            int left = st.empty() ? -1 : st.top();
+            ans = max(ans, height * (right - left - 1));
         }
-        return ans;
+        st.push(i);
     }
+    return ans;
+}
+
 };
