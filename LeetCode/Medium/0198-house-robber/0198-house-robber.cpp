ded04399cde9f94d4b59b731b35bf9740c8221c0
Sakibul Hasan <sakib1514817122@gmail.com>
2026-04-04 22:44:28 +0600

    0198-house-robber Medium 04-04-2026

--- LeetCode/Medium/0198-house-robber/0198-house-robber.cpp
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.cpp
@@ -4,13 +4,12 @@ public:
         if (nums.size() == 1) {
             return nums[0];
         }
-        int i, n;
-        n = nums.size();
+        int n = nums.size();
         vector<int> money(n);
         money[0] = nums[0];
         money[1] = max(nums[0], nums[1]);
 
-        for (i = 2; i < n; i++) {
+        for (int i = 2; i < n; i++) {
             money[i] = max(money[i - 1], nums[i] + money[i - 2]);
         }
         return money[n - 1];
